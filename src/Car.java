a3e1f8e7c5571a2cd8f7472be9033ac0e059d395
mariusbayizere <bayizeremarius119@gmail.com>
2025-10-20 08:44:30 +0200

    Push new

--- src/Car.java
+++ src/Car.java
@@ -1,8 +1,8 @@
 public class Car {
     public  static String name;
-    static String model;
-    static String year;
-    static String color;
+    String model="Mercedence";
+     String year="2025";
+     String color="Blue";
 
     public  void accerated(){
         System.out.println("Car are accerated !!!!");
@@ -14,19 +14,25 @@ public class Car {
     public void get_speed(){
         System.out.println("the car are getting the speed now!!!");
     }
+    public  static void  getNUmber(){
+        int a = (50+20) + (10*4) - 60;
+        System.out.println("Value of a is " + a);
+    }
+
 }
 
 
 class Test{
     public static void main(String[] args) {
-        Car.model = "test 1";
-        Car.color = "red";
-        Car.year= "2023";
         Car obj = new Car();
-        Car.name= "Marius Bayizere";
+        obj.name= "Marius Bayizere";
         obj.accerated();
         obj.brake();
         obj.get_speed();
+        System.out.println("Car has  " + obj.model + "Your car is working fine!!!!!!");
+        System.out.println("Car has  " + obj.year + "Your car is working fine!!!!!!");
+        System.out.println("Car has  " + obj.color + "Your car is working fine!!!!!!");
         System.out.println("Hello  " + Car.name + "Your car is working fine!!!!!!");
+        Car.getNUmber();
     }
 }
