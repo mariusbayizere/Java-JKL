828e343b90ab6900772a5ac61f1071adb6189f6a
mariusbayizere <bayizeremarius119@gmail.com>
2025-10-20 09:32:19 +0200

    Pushing the wrapper class

---
+++ src/Tests.java
@@ -0,0 +1,11 @@
+public class Tests {
+
+    //wrapper class
+    static byte intialnumbeer = Byte.MIN_VALUE;
+    static byte lastnumber = Byte.MAX_VALUE;
+
+    public static void main(String[] args) {
+        System.out.println("The Range the Byte datatye must be storing use :"+  intialnumbeer);
+        System.out.println("The Last number this Bytse datatype must be storing is :" + lastnumber);
+    }
+}
