9d7bd269407188d06a3f9c0afd4a6e8b23bc3028
mariusbayizere <bayizeremarius119@gmail.com>
2025-10-20 09:46:26 +0200

    Push new wrapper class Test

--- src/Tests.java
+++ src/Tests.java
@@ -1,11 +1,55 @@
 public class Tests {
 
     //wrapper class
-    static byte intialnumbeer = Byte.MIN_VALUE;
-    static byte lastnumber = Byte.MAX_VALUE;
+    /**
+     * this is method is showing the wrapper class of the Byte datatype
+     * return: void
+     */
+    public static void wrapperClassByte(){
+        byte intialnumbeer = Byte.MIN_VALUE;
+        byte lastnumber = Byte.MAX_VALUE;
+        System.out.println("The Range the Short datatye must be storing use it may start from :"+  intialnumbeer);
+        System.out.println("The Last number this Byte datatype must be storing is it will stop here :" + lastnumber);
+    }
+
+
+    /**
+     * this method is showing the wrapper class of short datatype
+     * return: void
+     */
+    public static void wrapperClassShort(){
+        short intialnumbeer = Short.MIN_VALUE;
+        short lastnumber = Short.MAX_VALUE;
+        System.out.println("The Range the Short datatye must be storing use it may start from  :"+  intialnumbeer);
+        System.out.println("The Last number this Short datatype must be storing is it will stop here :" + lastnumber);
+    }
+
+    /**
+     * this method is showing the wrapper class of the Integer datatype
+     * return: void
+     */
+    public static void wrapperClassInteger(){
+        int intialnumbeer = Integer.MIN_VALUE;
+        int lastnumber = Integer.MAX_VALUE;
+        System.out.println("The Range the Integer datatye must be storing use it may start from :"+  intialnumbeer);
+        System.out.println("The Last number this Integer datatype must be storing is it will stop here :" + lastnumber);
+    }
+
+    /**
+     * this method is showing the wrapper class of the long datatype
+     * return: void
+     */
+    public static void wrapperClassLong(){
+        long intialnumbeer = Long.MIN_VALUE;
+        long lastnumber = Long.MAX_VALUE;
+        System.out.println("The Range the Long datatye must be storing use it may start from :"+  intialnumbeer);
+        System.out.println("The Last number this Long datatype must be storing is it will stop here :" + lastnumber);
+    }
 
     public static void main(String[] args) {
-        System.out.println("The Range the Byte datatye must be storing use :"+  intialnumbeer);
-        System.out.println("The Last number this Bytse datatype must be storing is :" + lastnumber);
+        wrapperClassByte();
+        wrapperClassShort();
+        wrapperClassShort();
+        wrapperClassLong();
     }
 }
